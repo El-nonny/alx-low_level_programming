30d94980518f5ca572359d18fdd2c5dc3f90b8a0
El-nonny <chinonsoemmanuel41@gmail.com>
2022-03-14 14:47:42 +0100

    My commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Print the alphabet in lowercase letters, except for e and q
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		if (c != 'e' && c != 'q')
+		putchar(c);
+		}
+		putchar('\n');
+
+	return (0);
+}
