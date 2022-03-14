90b0fb0ff909f757e878f49fc0050ea9b3afe5de
El-nonny <chinonsoemmanuel41@gmail.com>
2022-03-14 09:48:34 +0100

    My commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.>
+
+   /*
+    * main - Entry point
+    *
+    * Return: Always 0
+    */
+
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle\n");
+  return (0);
+}
