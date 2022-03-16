a9b3aa3aa8e3224a4917e4299c71a96e1fe48506
El-nonny <chinonsoemmanuel41@gmail.com>
2022-03-16 14:48:13 +0100

    My commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints an alphabet 10 times in lower case
+ *
+ * Return void
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int i = 0;
+
+	for (i++ <= 9)
+	{
+		for (a = 'a'; alphabet <= 'z', a++);
+		_putchar(a);
+		_putchar('\n');	
+	}
+}
