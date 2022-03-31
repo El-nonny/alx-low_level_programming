855580ae30c3e6dd67acc3055dd6809831ee0739
El-nonny <chinonsoemmanuel41@gmail.com>
2022-03-31 01:13:57 +0100

    My commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,7 +6,6 @@
  * @s2: second string containing '*' or not
  * Return: 1 if the strings can be considered as identical, and 0
  **/
-
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,15 @@
  *
  * Return: The length of the string.
  */
-
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	return (_strien_recursion(s + 1) + 1);
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+
+	return (len);
 }
